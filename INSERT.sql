a1c2234d57d7acf47f71f5968c682ef8c2586afb
mauricio2093 <mauricio2093@gmail.com>
2022-06-11 22:25:16 -0500

    subiendo finalizado crate start INSERT

---
+++ INSERT.sql
@@ -0,0 +1,25 @@
+INSERT INTO authors(
+        author_id,
+        `name`,
+        nationality
+) 
+    VALUES(
+        '',
+        'Juan Rulfo',
+        'MEX',
+    );
+
+INSERT INTO authors(
+        `name`,
+        nationality
+) 
+    VALUES(
+        'Gabriel Garcia Marquez',
+        'COL',
+    );
+
+INSERT INTO authors
+    VALUES(
+        'Juan Gabriel Vasquez',
+        'COL',
+    );
